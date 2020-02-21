1c7d31712b5c5fb662d61c32891cdf65be5651dd
Unknown <tsuba-h@arsaga.jp>
2020-02-21 17:42:54 +0900

    firebase用のVC追加

---
+++ 303 Patterns Note/ViewController/FirebaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseViewController.swift
+//  303 Patterns Note
+//
+//  Created by 服部　翼 on 2020/02/21.
+//  Copyright © 2020 服部翼. All rights reserved.
+//
+
+import UIKit
+import Instantiate
+import InstantiateStandard
+
+class FirebaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
